479b14628fffbd8058a0a38de6144622d7b79274
Aya hanini <ayahanini255@gmail.com>
2025-03-28 11:59:07 +0000

    lecture du fichier csv

--- data.py
+++ data.py
@@ -4,3 +4,5 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import f1_score, classification_report
+df = pd.read_csv('D:/CHATBOTMEDICAL2/PFA/heart.csv')
+print(df.head())
