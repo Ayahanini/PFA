88287de4db671861f319c6a4fcbaa8cf89bf2d61
Aya hanini <ayahanini255@gmail.com>
2025-03-28 11:46:25 +0000

    my first commit

---
+++ data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score, classification_report
