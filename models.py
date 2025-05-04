2bd65350d88ab1bf1ce765b49904e8b8d6c005ad
kawtarARR <k.arrami@esisa.ac.ma>
2025-05-04 18:08:03 +0100

    entrainer le modele

--- models.py
+++ models.py
@@ -11,4 +11,16 @@ def charger_donnees(fichier):
     X = df.drop(columns=['target'])  # Supposons que la colonne cible s'appelle 'target'
     y = df['target']
     return X, y
+def entrainer_modele(fichier):
+    """Entraîne le modèle et le sauvegarde dans un fichier."""
+    X, y = charger_donnees(fichier)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    modele = Pipeline([
+        ('scaler', StandardScaler()),
+        ('clf', LogisticRegression())
+    ])
+    modele.fit(X_train, y_train)
+    joblib.dump(modele, 'modele_heart.pkl')
+    print("Modèle entraîné et sauvegardé.")
+
 
