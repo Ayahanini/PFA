922b2c1da12798f4e820084063b768d7c38670bb
kawtarARR <k.arrami@esisa.ac.ma>
2025-05-04 18:43:27 +0100

    creation de logger

--- utils.py
+++ utils.py
@@ -10,3 +10,25 @@ logging.basicConfig(
     ]
 )
 
+# Création d'un logger
+logger = logging.getLogger(__name__)
+
+def log_message(message, level="info"):
+    """
+    Enregistre un message dans les logs.
+
+    Args:
+        message (str): Le message à enregistrer.
+        level (str): Le niveau du log (info, warning, error, critical).
+    """
+    if level == "info":
+        logger.info(message)
+    elif level == "warning":
+        logger.warning(message)
+    elif level == "error":
+        logger.error(message)
+    elif level == "critical":
+        logger.critical(message)
+    else:
+        logger.debug(message)
+
