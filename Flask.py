e6c2f37c5331799bcf719e7e61eab61c9a430406
kawtarARR <k.arrami@esisa.ac.ma>
2025-05-05 20:58:00 +0100

    flask css

--- Flask.py
+++ Flask.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from knowledge_base import kb
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
@@ -10,101 +14,10 @@ def ask_question():
     user_data = data.get('user_data', {})
     
     if not question:
-        return jsonify({"error": "Aucune question fournie."}), 400
+        return jsonify({"response": "Aucune question fournie."}), 400
     
     response = kb.answer_question(question, user_data)
-    return jsonify(response)
+    return jsonify({"response": response["response"]})
 
 if __name__ == '__main__':
     app.run(debug=True)
-    from flask import Flask, request, jsonify, render_template
-    from knowledge_base import kb
-    from flask import Flask, request, jsonify, render_template
-    from knowledge_base import kb
-    
-    app = Flask(__name__, template_folder='templates')
-    
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-    
-    @app.route('/ask', methods=['POST'])
-    def ask_question():
-        data = request.json
-        question = data.get('question', '')
-        user_data = data.get('user_data', {})
-        
-        if not question:
-            return jsonify({"error": "Aucune question fournie."}), 400
-        
-        response = kb.answer_question(question, user_data)
-        return jsonify(response)
-    
-    if __name__ == '__main__':
-        app.run(debug=True)
-        from flask import Flask, request, jsonify, render_template
-        from flask import Flask, request, jsonify, render_template
-        from knowledge_base import kb
-        
-        app = Flask(__name__, template_folder='templates')
-        
-        @app.route('/')
-        def index():
-            return render_template('index.html')
-        
-        @app.route('/ask', methods=['POST'])
-        def ask_question():
-            data = request.json
-            question = data.get('question', '')
-            user_data = data.get('user_data', {})
-            
-            if not question:
-                return jsonify({"error": "Aucune question fournie."}), 400
-            
-            response = kb.answer_question(question, user_data)
-            return jsonify(response)
-        
-        if __name__ == '__main__':
-            app.run(debug=True)        
-            from knowledge_base import kb
-        
-        app = Flask(__name__, template_folder='templates')
-        
-        @app.route('/')
-        def index():
-            return render_template('index.html')
-        
-        @app.route('/ask', methods=['POST'])
-        def ask_question():
-            data = request.json
-            question = data.get('question', '')
-            user_data = data.get('user_data', {})
-            
-            if not question:
-                return jsonify({"error": "Aucune question fournie."}), 400
-            
-            response = kb.answer_question(question, user_data)
-            return jsonify(response)
-        
-        if __name__ == '__main__':
-            app.run(debug=True)
-    app = Flask(__name__, template_folder='templates')
-    
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-    
-    @app.route('/ask', methods=['POST'])
-    def ask_question():
-        data = request.json
-        question = data.get('question', '')
-        user_data = data.get('user_data', {})
-        
-        if not question:
-            return jsonify({"error": "Aucune question fournie."}), 400
-        
-        response = kb.answer_question(question, user_data)
-        return jsonify(response)
-    
-    if __name__ == '__main__':
-        app.run(debug=True)
