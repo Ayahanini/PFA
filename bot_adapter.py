83f6bbfc1ea625f414d93dda70f72f91d88e0bc3
Aya hanini <ayahanini255@gmail.com>
2025-06-02 20:26:50 +0100

    bot framework dans mon application

---
+++ bot_adapter.py
@@ -0,0 +1,25 @@
+import os
+from botbuilder.core import (
+    BotFrameworkAdapter,
+    BotFrameworkAdapterSettings,
+    MemoryStorage,
+    ConversationState,
+    UserState,
+)
+from bot import MedicalBot
+from knowledge_base import KnowledgeBase
+
+APP_ID = os.environ.get("MicrosoftAppId", "")
+APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
+SETTINGS = BotFrameworkAdapterSettings(APP_ID, APP_PASSWORD)
+ADAPTER = BotFrameworkAdapter(SETTINGS)
+
+MEMORY = MemoryStorage()
+CONVERSATION_STATE = ConversationState(MEMORY)
+USER_STATE = UserState(MEMORY)
+
+# Charger la base de connaissances
+KB = KnowledgeBase("D:/CHATBOTMEDICAL2/PFA/medical_knowledge.json")
+
+BOT = MedicalBot()
